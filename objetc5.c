0b8082f24c6e1376af7f42040b5f79638d5825d7
Asjal Amir <113906443+mohdasjal1@users.noreply.github.com>
2023-12-14 23:34:52 +0500

    Add files via upload

---
+++ objetc5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void findAndPrint(const char *string)
+{
+    if (*string == '\0')
+    {
+        printf("No capital letter found in the string.\n");
+        return;
+    }
+
+    if (*string >= 'A' && *string <= 'Z')
+    {
+        printf("First capital is %c\n", *string);
+        return;
+    }
+
+    findAndPrint(string + 1);
+}
+
+int main()
+{
+    char string[500];
+
+    printf("Enter a string : \n");
+    fgets(string, sizeof(string), stdin);
+
+    findAndPrint(string);
+
+    return 0;
+}
